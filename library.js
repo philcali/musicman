fa9951fd7adffdef25d2a503b2762b26a2467420
Philip Cali <philip.cali@gmail.com>
2011-08-21 11:09:01 -0500

    Can control across all of chrome, woot

--- library.js
+++ library.js
@@ -1,8 +1,12 @@
 function withMusicTab(doTab) {
-  chrome.tabs.getAllInWindow(null, function(tabs) {
-    for (var i = 0; i < tabs.length; i ++) {
-      if (tabs[i].url.match(/music.google.com/)) {
-        doTab(tabs[i]);
+  chrome.windows.getAll({ populate: true }, function(windows) {
+    for (var j = 0; j < windows.length; j ++) {
+      var tabs = windows[j].tabs;
+
+      for (var i = 0; i < tabs.length; i ++) {
+        if (tabs[i].url.match(/music.google.com/)) {
+          doTab(tabs[i]);
+        }
       }
     }
   });
